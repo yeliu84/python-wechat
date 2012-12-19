4584f413b84637e7f9f0800329b6f59b387fcdde
Ye Liu <yeliu@instast.com>
2012-12-19 02:53:42 -0500

    v0.0.2
    * Rename getlog script to wechatlog

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import wechat
 setup(name='wechat',
       description='Interfaces and utilities for WeChat database',
       packages=['wechat'],
-      scripts=['scripts/getlog'],
+      scripts=['scripts/wechatlog'],
       version=wechat.__version__,
       license=wechat.__license__,
       author=wechat.__author__,
--- wechat/__init__.py
+++ wechat/__init__.py
@@ -7,8 +7,8 @@ wechat
 This module provides interfaces to various parts of the WeChat database.
 """
 
-__version__ = '0.0.1'
-__ver__ = (0, 0, 1)
+__version__ = '0.0.2'
+__ver__ = (0, 0, 2)
 __author__ = 'Ye Liu'
 __contact__ = 'yeliu@instast.com'
 __license__ = 'BSD'
