fc09b8e52f56b8f3b70caecf6332a05bbc8a54a0
Ye Liu <yeliu@instast.com>
2012-12-19 02:50:20 -0500

    getlog scripts and setup.py.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+import wechat
+
+setup(name='wechat',
+      description='Interfaces and utilities for WeChat database',
+      packages=['wechat'],
+      scripts=['scripts/getlog'],
+      version=wechat.__version__,
+      license=wechat.__license__,
+      author=wechat.__author__,
+      author_email=wechat.__contact__,
+      url='http://wechatlog.com')
--- wechat/__init__.py
+++ wechat/__init__.py
@@ -10,5 +10,8 @@ This module provides interfaces to various parts of the WeChat database.
 __version__ = '0.0.1'
 __ver__ = (0, 0, 1)
 __author__ = 'Ye Liu'
+__contact__ = 'yeliu@instast.com'
 __license__ = 'BSD'
 __copyright__ = 'Copyright (c) 2012 Ye Liu'
+
+__all__ = ['db', 'conversation', 'friend']
